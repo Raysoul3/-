7baa8d7cf42e172a71350ad40adfb55854154de9
Raysoul3 <wuhuilei3@gmail.com>
2023-09-04 19:54:09 +0900

    連結の追加した

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -3,16 +3,23 @@ package model;
 import java.io.Serializable;
 
 public class User implements Serializable {
+	private int id;
 	private String name;
 	private String pass;
 
-	public User() {
+	public User(int id, String name, String pass) {
+		this(name, pass);
+		this.id = id;
 	}
 
 	public User(String name, String pass) {
 		this.name = name;
 		this.pass = pass;
 
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String getName() {
